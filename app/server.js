dd10c7e095a7a9f1fb7cd706e0f55eb320430711
Ubuntu <ubuntu@ip-172-31-23-205.ec2.internal>
2025-10-15 13:49:32 +0000

    new test

--- app/server.js
+++ app/server.js
@@ -1,14 +1,20 @@
 // app/server.js
 import express from "express";
+import os from "os";
 
 const app = express();
 const PORT = 8080;
 
+// Get this machine's hostname (unique per EC2 instance)
+const hostname = os.hostname();
+
 app.get("/", (req, res) => {
-  res.send("Server is running inside Docker on port 8080 🚀. This is version 3");
+  res.send(`Server is running inside Docker on port 8080 🚀. 
+This is version 3.
+Served from instance: ${hostname}`);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT} (instance: ${hostname})`);
 });
 
