1abbc233222af1b4aba09b9e2c29123257ec071b
Ubuntu <ubuntu@ip-172-31-29-110.ec2.internal>
2025-10-16 14:03:57 +0000

    deploy test

--- app/server.js
+++ app/server.js
@@ -4,17 +4,20 @@ import os from "os";
 
 const app = express();
 const PORT = 8080;
-
-// Get this machine's hostname (unique per EC2 instance)
 const hostname = os.hostname();
 
+// Generate a random ID once when the container starts
+const instanceID = Math.floor(Math.random() * 1000000);
+
 app.get("/", (req, res) => {
-  res.send(`Server is running inside Docker on port 8080 🚀. 
-This is version 3.
-Served from instance: ${hostname}`);
+  res.send(`
+    Server is running inside Docker on port 8080 🚀 <br>
+    Unique instance ID: ${instanceID} <br>
+    Container hostname: ${hostname}
+  `);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} (instance: ${hostname})`);
+  console.log(`Server is running on port ${PORT}, instance ID: ${instanceID}`);
 });
 
